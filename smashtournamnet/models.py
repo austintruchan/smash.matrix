07d8085cc18dc4aadfd496898f14542a47adc834
Jake Lange <lanjak11@gmail.com>
2014-09-15 14:44:02 -0400

    new models and added to admin interface

--- smashtournamnet/models.py
+++ smashtournamnet/models.py
@@ -2,16 +2,24 @@ from django.db import models
 
 class Version(models.Model):
 	name = models.CharField(max_length=200)
-	characters = models.OneToManyField(Character, verbose_name="list of characters")
+	
+	def __str__(self):
+		return self.name
 
 class Player(models.Model):
 	name = models.CharField(max_length=200)
-	date_created = models.DateTimeField('date joined')
+	date_created = models.DateTimeField()
+
+	def __str__(self):
+		return self.name
 
 class Character(models.Model):
 	name = models.CharField(max_length=200)
-	version = models.OneToManyField(Version, verbose_name="version of smash")
+	version = models.ForeignKey(Version) 
+
+	def __str__(self):
+		return self.name
 
 class Tournament(models.Model):
-	players = models.OneToManyField(Player, verbose_name="participants")
+	players = models.ManyToManyField(Player)
 		
