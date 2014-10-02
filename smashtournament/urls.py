d84bfdd6d86bf232e22a4cc9809946b08ddca23a
Jake Lange <lanjak11@gmail.com>
2014-10-02 12:46:09 -0400

    added some basic routes and htmls

--- smashtournament/urls.py
+++ smashtournament/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from smashtournament import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^leaders/', views.leaders, name='leaders'),
+	url(r'^match/', views.match, name='match'),
 )
--- smashtournament/views.py
+++ smashtournament/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-def index(response):
-	return HttpResponse("Hello world")
+def index(request):
+	template = loader.get_template('smashtournament/index.html')
+	context = RequestContext(request)	
+	return HttpResponse(template.render(context)) 
+
+def leaders(request):
+	template = loader.get_template('smashtournament/leaders.html')
+	context = RequestContext(request)
+	return HttpResponse(template.render(context))
+
+def match(request):
+	template = loader.get_template('smashtournament/match.html')
+	context = RequestContext(request)
+	return HttpResponse(template.render(context))
+
